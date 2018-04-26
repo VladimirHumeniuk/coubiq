4ab82e51460200ba2f19e84f18d37c952dee8004
Vladimir Humeniuk <vg6962@gmail.com>
2018-04-26 13:52:52 +0300

    fix: add env to ignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAlu34aQ5DYG4BmvIB5bHGuzSu-VA9fRfM",
-    authDomain: "payments-mp.firebaseapp.com",
-    databaseURL: "https://payments-mp.firebaseio.com",
-    projectId: "payments-mp",
-    storageBucket: "payments-mp.appspot.com",
-    messagingSenderId: "782868182932"
+    apiKey: "AIzaSyB7YdHDTO-GjjQZe0IQV_gjWlfqk558p_U",
+    authDomain: "coubiq-app.firebaseapp.com",
+    databaseURL: "https://coubiq-app.firebaseio.com",
+    projectId: "coubiq-app",
+    storageBucket: "",
+    messagingSenderId: "96387933724"
   }
 };

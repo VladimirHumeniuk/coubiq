d2cf349c3eaac08d6bdf287a6da09f004c210faf
Vladimir Humeniuk <vg6962@gmail.com>
2018-05-01 23:40:19 +0300

    feat: add signup page template

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
+  public background: string = '../../../assets/images/bg_sign-in.png';
+
   constructor() { }
 
   ngOnInit() {

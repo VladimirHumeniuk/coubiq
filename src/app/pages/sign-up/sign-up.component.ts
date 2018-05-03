ce7c79adb78abc53beeb27cd56b2c8ac552b115a
Vladimir Humeniuk <vg6962@gmail.com>
2018-05-03 04:33:08 +0300

    feat: add some signup validation

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { RegistrationService } from "../../shared/services/registration.service";
+import { EqualValidator } from '../../shared/directives/validate-equal.directive';
 
 @Component({
   selector: 'app-sign-up',
@@ -13,6 +14,24 @@ export class SignUpComponent implements OnInit {
   public background: string = '../../../assets/images/bg_sign-in.png';
   public registration: FormGroup;
 
+  private emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  public invalidEmail: string = 'Эмейл не валидный.';
+  public passwordNotEqual: string = 'Пароли не совпадают.';
+
+  protected nameMinLength: number = 4;
+  protected nameMaxLength: number = 36;
+  protected passMinLength: number = 6;
+  protected passMaxLength: number = 30;
+
+  public selectedCountry: string = 'Украина';
+  public selectedCity: string;
+
+  public countryData = ['Украина'];
+  public cityData = {
+    Украина: ['Киев', 'Харьков', 'Львов']
+  };
+
   constructor(
     private fb: FormBuilder,
     public regService: RegistrationService
@@ -20,12 +39,51 @@ export class SignUpComponent implements OnInit {
 
   initForm() {
     this.registration = this.fb.group({
-      username: [''],
-      email: [''],
-      password: [''],
+      username: ['', [
+        Validators.required,
+        Validators.minLength(this.nameMinLength),
+        Validators.maxLength(this.nameMaxLength)
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern(this.emailRegex)
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(this.passMinLength),
+        Validators.maxLength(this.passMaxLength)
+      ]],
+      passwordConfirm: ['', [
+        Validators.required,
+        EqualValidator('password')
+      ]],
       country: [],
-      city: []
+      city: [Validators.required]
     })
+  }
+
+  isControlInvalid(controlName: string): boolean {
+    const control = this.registration.controls[controlName];
+    const result = control.invalid && control.touched;
+
+    return result;
+  }
+
+  minLengthError(inputName: string, minlength: number) {
+    const sufix = minlength < 5 ? "а" : "ов";
+    return `Минимальная длина ${inputName}: ${minlength} символ${sufix}.`
+  }
+
+  countryChange(value: string): void {
+    this.selectedCity = this.cityData[value][0];
+  }
+
+  submitForm(): void {
+    for (const i in this.registration.controls) {
+      this.registration.controls[i].markAsDirty();
+      this.registration.controls[i].updateValueAndValidity();
+    }
   }
 
   ngOnInit() {
